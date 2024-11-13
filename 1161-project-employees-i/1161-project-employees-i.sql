f81fe1e8d8f27feaf815cb6d241a4d6fff88087d
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2024-11-13 13:11:22 +0530

    Time: 603 ms (41.3%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,6 @@
+-- avg(exp_yr), round 2, by project
+SELECT project_id, ROUND(AVG(experience_years), 2) as average_years
+FROM Project p 
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY project_id
