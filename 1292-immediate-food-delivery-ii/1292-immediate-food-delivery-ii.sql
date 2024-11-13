080ddf99e8e75289555410acbbbbdbaf1afc479b
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2024-11-13 21:46:51 +0530

    Time: 1029 ms (13.66%), Space: 0B (100%) - LeetHub

---
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -0,0 +1,5 @@
+SELECT ROUND(AVG(temp.order_date=temp.customer_pref_delivery_date) * 100, 2) immediate_percentage
+FROM (
+    SELECT *, RANK() OVER(partition by customer_id ORDER BY order_date) od
+    FROM Delivery) temp
+WHERE temp.od = 1
