363a45555d74c4085a592be0c8fa4aad38735d3d
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2024-11-13 21:38:49 +0530

    Time: 496 ms (16.79%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,5 @@
+SELECT query_name, 
+    ROUND(AVG(rating/position), 2) AS quality, 
+    ROUND(SUM(IF(rating < 3, 1, 0)) * 100/ COUNT(rating), 2) AS poor_query_percentage
+FROM Queries where query_name IS NOT NULL
+GROUP BY query_name
