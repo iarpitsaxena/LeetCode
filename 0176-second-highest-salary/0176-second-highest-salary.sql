fda14ed6e314e54043df1d62f90747dac4af78b9
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2024-11-16 09:54:54 +0530

    Time: 258 ms (92.98%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,8 @@
+SELECT
+(SELECT DISTINCT Salary 
+FROM Employee
+ORDER BY Salary DESC
+LIMIT 1 OFFSET 1)
+AS SecondHighestSalary
+
+-- HINT: subquery is used to return null if there is no SecondHighestSalary
