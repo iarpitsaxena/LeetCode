3fc7a343789a16a2e5671b20c9eb8d5554b1d321
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-02-26 00:17:26 +0530

    Time: 6 ms (48.96%), Space: 69.1 MB (13.92%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -7,36 +7,35 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- 
+ */
 class Solution {
 
-    public ListNode middleNode(ListNode head){
+    public ListNode middleNode(ListNode head) {
         ListNode s = head;
         ListNode f = head;
 
-        while(f != null && f.next!= null){
-            s= s.next;
+        while (f != null && f.next != null) {
+            s = s.next;
             f = f.next.next;
         }
         return s;
     }
 
-    public ListNode reverseList(ListNode head){
-        if(head == null){
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
             return head;
         }
-
         ListNode prev = null;
         ListNode present = head;
         ListNode next = present.next;
 
-        while(present != null){
+        while (present != null) {
             present.next = prev;
             prev = present;
             present = next;
-        }
-        if(next != null){
-            next = next.next;
+            if (next != null) {
+                next = next.next;
+            }
         }
         return prev;
     }
@@ -46,61 +45,17 @@ class Solution {
         ListNode headSecond = reverseList(mid);
         ListNode rereverseHead = headSecond;
 
-        while(head != null && headSecond != null){
-            if(head.val != headSecond.val){
+        // compare both the halves
+        while (head != null && headSecond != null) {
+            if (head.val != headSecond.val) {
                 break;
             }
             head = head.next;
             headSecond = headSecond.next;
         }
         reverseList(rereverseHead);
+
         return head == null || headSecond == null;
-        
     }
 }
-*/
 
-class Solution {
-
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode present = head;
-
-        while (present != null) {
-            ListNode next = present.next; // Store next node
-            present.next = prev;          // Reverse link
-            prev = present;               // Move prev forward
-            present = next;               // Move present forward
-        }
-        return prev; // New head of reversed list
-    }
-
-    public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) return true; // Edge case
-
-        ListNode mid = middleNode(head);
-        ListNode headSecond = reverseList(mid); // Reverse second half
-        ListNode firstHalf = head;
-
-        while (headSecond != null) { // Only iterate through the second half
-            if (firstHalf.val != headSecond.val) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            headSecond = headSecond.next;
-        }
-
-        return true;
-    }
-}
