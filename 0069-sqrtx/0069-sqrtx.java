3ac60b889d743d19ef53f792f8ebad59ccc932cd
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-03-20 09:30:40 +0530

    Time: 1 ms (86.22%), Space: 41 MB (52.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,19 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) {
-            return x;
-        }
-        int first = 1, last = x;
-        while (first <= last) {
-            int mid = first + (last - first) / 2;
-            if (mid == x / mid) {
-                return mid;
-            } else if (mid > x / mid) {
-                last = mid - 1;
-            } else {
-                first = mid + 1;
-            }
-        }
-        return last;
+        if(x == 0 || x ==1) return x;
+    
+    int first = 1;
+    int last = x;
+    while(first<=last){
+        int mid = first + (last - first)/2;
+        if(mid == x/mid) return mid;
+        else if(mid > x/mid) last = mid - 1;
+        else first = mid + 1;
+    }
+    return last;
     }
 }
