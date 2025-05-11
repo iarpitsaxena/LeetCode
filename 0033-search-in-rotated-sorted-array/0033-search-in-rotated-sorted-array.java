a2ce4d8d3e5fcc28d6b76bbef3efb0fdd639fab3
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-05-11 17:32:01 +0530

    Time: 0 ms (100%), Space: 41.9 MB (92.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,55 +1,28 @@
-// iterative method
-// class Solution {
-//     public int search(int[] nums, int target) {
-//         int l = 0;
-//         int r  = nums.length -1;
-
-//         while(l<=r){
-//             final int m = (l+r)/2;
-//             if(nums[m] == target)
-//                 return m;
-
-//             if(nums[l] <= nums[m]){
-//                 if(nums[l] <= target && target < nums[m])
-//                     r = m -1;
-//                 else
-//                     l = m + 1;
-//             }
-//             else{
-//                 if(nums[m] < target && target <= nums[r]){
-//                     l = m + 1;
-//                 }
-//                 else r = m - 1;
-//             }
-//         }
-//         return -1;
-        
-//     }
-// }
-
-// recursive method
-class Solution{
+class Solution {
     public int search(int[] nums, int target) {
-        return search( nums, target, 0, nums.length -1);
-    }
+        int l = 0;
+        int r = nums.length - 1;
 
-    private int search(int[] nums, int target, int l , int r){
-        if(l > r) return -1;
+        while(l <= r){
+            final int m = (l + r) / 2;
+            if(nums[m] == target) return m;
 
-        final int m = (l + r)/ 2;
-        if(nums[m] == target) return m;
-
-        if(nums[l]<=nums[m]){
-        if(nums[l] <= target && target < nums[m])
-            return search(nums,target,l, m - 1);
-        else
-            return search(nums, target , m + 1 , r);
-        }else{
-            if(nums[m] < target && target <= nums[r])
-                return search(nums, target , m + 1, r);
-            else
-                return search(nums,target,l, m - 1);
+            if(nums[l] <= nums[m]){
+                if(nums[l] <= target && target < nums[m])
+                    r = m -1;
+                else
+                    l = m + 1;
+            }
+            else{
+                if(nums[m] < target && target  <= nums[r]){
+                    l = m + 1;
+                }
+                else r = m -1;
+            }
         }
+        return -1;
 
+        
+        
     }
 }
