20359314ecb6bec3ee84b56ec039a8ab80e779be
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-05-16 21:35:30 +0530

    Time: 0 ms (100%), Space: 41.3 MB (85.34%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
@@ -15,39 +15,30 @@
  */
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
-        TreeNode xx = findNode(root, x);
-        TreeNode yy = findNode(root, y);
-
-        return(
-                (level(root,xx,0) == level(root,yy,0)) && (!isSibling(root,xx,yy))
-                );
-    }
-
-    TreeNode findNode(TreeNode node, int x){
-        if(node == null) return null;
-        if(node.val == x) return node;
-
-        TreeNode n = findNode(node.left, x);
-        if(n != null){
-            return n;
+        if(root == null) return false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            boolean isxExist = false;
+            boolean isyExist = false;
+            for(int i = 0; i < size;i++){
+                TreeNode curr = queue.poll();
+                if(curr.val == x) isxExist = true;
+                if(curr.val == y) isyExist = true;
+                if(curr.left != null && curr.right != null){
+                    if(curr.left.val == x && curr.right.val == y){
+                        return false;
+                    }
+                    if(curr.left.val == y && curr.right.val == x){
+                        return false;
+                    }
+                }
+                if(curr.left != null) queue.offer(curr.left);
+                if(curr.right != null) queue.offer(curr.right);
+            }
+            if(isxExist && isyExist) return true;
         }
-        return findNode(node.right, x);
-    }
-
-    boolean isSibling(TreeNode node, TreeNode x,TreeNode y){
-        if(node == null) return false;
-
-        return(node.left == x && node.right == y) || (node.left == y && node.right == x)
-                || isSibling(node.left, x, y) || isSibling(node.right, x, y);
-    }
-
-    int level(TreeNode node, TreeNode x, int lev){
-        if(node == null) return 0;
-
-        if(node == x) return lev;
-
-        int l = level(node.left,x,lev + 1);
-        if(l != 0) return l;
-        return level(node.right,x,lev + 1);
+        return false;
     }
 }
