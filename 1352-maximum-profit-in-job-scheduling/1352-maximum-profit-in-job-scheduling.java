3c3b301e15162247316bc6ca0db6628f387489a7
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-07-03 07:26:05 +0530

    Time: 60 ms (26.12%), Space: 55.9 MB (75.84%) - LeetHub

---
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.java
@@ -0,0 +1,37 @@
+class Solution {
+    private record Job(int startTime, int endTime, int profit){}
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        final int n = startTime.length;
+
+        Job[] jobs = new Job[n];
+
+        for(int i = 0;i< n;++i){
+            jobs[i] = new Job(startTime[i],endTime[i],profit[i]);// yha basically aap ek naya object bna rhe ho and then sorting it.
+        }
+
+        Arrays.sort(jobs,Comparator.comparingInt(Job::startTime));// This line sorts the array of Job objects in ascending order of their startTime.
+                                                                  //Comparator<Job> cmp = (Job j) -> j.startTime();
+
+        for(int i = 0;i<n ;i++){
+            startTime[i] = jobs[i].startTime;
+        }
+
+        return getMaxProfit(jobs);  
+    }
+
+    private int getMaxProfit(Job[] jobs){
+        int maxProfit = 0;
+        Queue<Job> minHeap = new PriorityQueue<>(Comparator.comparingInt(Job::endTime));
+        for (Job job: jobs){
+            while(!minHeap.isEmpty() && job.startTime>=minHeap.peek().endTime)
+                maxProfit = Math.max(maxProfit,minHeap.poll().profit());
+            minHeap.offer(new Job(job.startTime,job.endTime,job.profit+maxProfit));
+        }
+
+        while(!minHeap.isEmpty()){
+            maxProfit = Math.max(maxProfit,minHeap.poll().profit);
+        }
+        return maxProfit;
+    }
+}
