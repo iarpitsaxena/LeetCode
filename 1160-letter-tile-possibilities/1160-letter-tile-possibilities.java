6ad2b9f24322fee4cb85aa12139151f6f93ca1f2
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-07-06 23:21:22 +0530

    Time: 30 ms (11.7%), Space: 45.7 MB (19.69%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        Set<String> sequences = new HashSet<>();
+        int len = tiles.length();
+        boolean[] used = new boolean[len];
+
+        generateSequences(tiles,"",used,sequences);   
+        return sequences.size() - 1;     
+    }
+
+    public void generateSequences(String tiles,String current,boolean[] used,Set<String> sequences){
+
+        sequences.add(current);
+        for(int i =0;i < tiles.length();i++){
+            if(!used[i]){
+                used[i] = true;
+                generateSequences(tiles,current + tiles.charAt(i),used,sequences);
+                used[i]=false;  
+            }
+        }
+    }
+}
