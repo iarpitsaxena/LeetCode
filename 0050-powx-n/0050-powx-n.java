741b6d332e26670b1946319af593d8efbff6a872
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-07-06 21:05:10 +0530

    Time: 0 ms (100%), Space: 43.7 MB (10.27%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
-        
+        return binaryExp(x,(long)n);        
     }
+
+    private static double binaryExp(double x, long n){
+        if(n == 0) return 1;
+        if(n < 0){
+            return 1.0/binaryExp(x,-1*n);
+        }
+
+        if(n%2 == 1){
+            return x * binaryExp(x*x,n/2);
+        }
+        else{
+            return binaryExp(x*x,n/2);
+        }
+    } 
 }
