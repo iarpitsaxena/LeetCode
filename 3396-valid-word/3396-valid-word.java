4d2a47c2735e3d4360cfaf1712c8f9d5df8b8bb7
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-07-15 22:45:24 +0530

    Time: 1 ms (99.12%), Space: 41.8 MB (90.73%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length()<3) return false;
+        boolean hasVowel = false;
+        boolean hasCons = false;
+        for(char c:word.toCharArray()){
+            if(Character.isLetter(c)){
+                char ch = Character.toLowerCase(c);
+                if(
+                ch =='a'||
+                ch=='e' ||
+                ch=='i' ||
+                ch=='o' ||
+                ch=='u'){
+                    hasVowel = true;
+                }else hasCons = true;
+            }
+            else if(!Character.isDigit(c)){
+                return false;
+            }
+        }
+        return hasVowel && hasCons;
+    }
+}
