083fe38f64c7c68e7aafe237c89fb9c94388f794
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-09-01 18:39:33 +0530

    Time: 5 ms (56.25%), Space: 58.2 MB (37.5%) - LeetHub

---
+++ 3694-minimum-total-operations/3694-minimum-total-operations.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        if(nums.length == 1) return 0;
+        for(int i = nums.length - 2;i >= 0;i--){
+            if(nums[i]!= nums[i+1]) count++;
+        }
+        return count;
+        
+    }
+}
