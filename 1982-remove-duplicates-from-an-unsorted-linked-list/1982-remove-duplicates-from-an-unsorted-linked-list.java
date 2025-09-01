557953ed5a2ad8e6fa46f1fb802bcba75bd4cbd4
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-09-01 20:13:34 +0530

    Time: 74 ms (50.68%), Space: 62.1 MB (78.08%) - LeetHub

---
+++ 1982-remove-duplicates-from-an-unsorted-linked-list/1982-remove-duplicates-from-an-unsorted-linked-list.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode deleteDuplicatesUnsorted(ListNode head) {
+        ListNode dummy = new ListNode(0,head);
+        Map<Integer,Integer> hash = new HashMap<>();
+        for(ListNode curr = head; curr != null;curr=curr.next){
+            hash.merge(curr.val,1,Integer::sum);
+        }
+        ListNode curr = dummy;
+        while(curr!=null){
+            while(curr.next!=null && hash.containsKey(curr.next.val)&&hash.get(curr.next.val)>1){
+                curr.next = curr.next.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
