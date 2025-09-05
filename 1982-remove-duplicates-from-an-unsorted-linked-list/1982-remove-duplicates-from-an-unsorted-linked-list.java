dc1f6a5f3360ac311309e559b7bcc02a73e263fb
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2025-09-05 19:11:35 +0530

    Time: 93 ms (5.48%), Space: 66 MB (20.55%) - LeetHub

--- 1982-remove-duplicates-from-an-unsorted-linked-list/1982-remove-duplicates-from-an-unsorted-linked-list.java
+++ 1982-remove-duplicates-from-an-unsorted-linked-list/1982-remove-duplicates-from-an-unsorted-linked-list.java
@@ -1,23 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode deleteDuplicatesUnsorted(ListNode head) {
         ListNode dummy = new ListNode(0,head);
-        Map<Integer,Integer> hash = new HashMap<>();
-        for(ListNode curr = head; curr != null;curr=curr.next){
-            // hash.merge(curr.val,1,Integer::sum);
-            if(hash.containsKey(curr.val)){
-                hash.put(curr.val,hash.get(curr.val)+1);
+        Map<Integer,Integer> map = new HashMap<>();
+        for(ListNode curr = head; curr!=null;curr= curr.next){
+            if(map.containsKey(curr.val)){
+                map.put(curr.val,map.get(curr.val)+1);
             }
-            else{
-                hash.put(curr.val,1);
-            }
+            else map.put(curr.val,1);
         }
         ListNode curr = dummy;
         while(curr!=null){
-            while(curr.next!=null && hash.containsKey(curr.next.val)&&hash.get(curr.next.val)>1){
-                curr.next = curr.next.next;
+            while(curr.next!=null && map.containsKey(curr.next.val)&&map.get(curr.next.val)>1){
+            curr.next = curr.next.next;
             }
             curr = curr.next;
         }
+        
         return dummy.next;
+
     }
 }
