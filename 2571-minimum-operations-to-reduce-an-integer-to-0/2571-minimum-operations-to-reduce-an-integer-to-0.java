9a48cc62e9ce56d05d1231e9d7ee81cae8345041
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-02-09 21:13:22 +0530

    Time: 0 ms (100%), Space: 42.4 MB (8.79%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minOperations(int n) {
+        if( n== 0 || n ==1) return n;
+        int val = 1;
+        while(val * 2 < n){
+            val = val * 2;
+        }
+        return 1 + Math.min(minOperations(val*2 - n), minOperations(n - val));
+    }
+}
