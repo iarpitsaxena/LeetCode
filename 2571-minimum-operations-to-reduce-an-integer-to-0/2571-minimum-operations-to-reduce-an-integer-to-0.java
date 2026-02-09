34658b05cf62594bed9c83695d74132a0f1417bb
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-02-09 22:16:15 +0530

    Time: 1 ms (11.88%), Space: 42 MB (69.18%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.java
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.java
@@ -1,10 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
+
+    Map<Integer, Integer> dp = new HashMap<>();
+
     public int minOperations(int n) {
-        if( n== 0 || n ==1) return n;
-        int val = 1;
-        while(val * 2 < n){
-            val = val * 2;
+        if (n == 0 || n == 1) return n;
+
+        if (dp.containsKey(n)) return dp.get(n);
+
+        int val = Integer.highestOneBit(n);
+        if(n == val) {
+            dp.put(n,1);
+            return 1;
         }
-        return 1 + Math.min(minOperations(val*2 - n), minOperations(n - val));
+        int ans = 1 + Math.min(
+            minOperations(val * 2 - n),
+            minOperations(n - val)
+        );
+
+        dp.put(n, ans);
+        return ans;
     }
 }
