549d788828b6138ad457886eb39c804d967c35e6
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-05-29 13:33:17 +0530

    Time: 87 ms (56.62%), Space: 47 MB (69.15%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
@@ -1,62 +1,22 @@
 class Solution {
-
     public long maximumProfit(int[] prices, int k) {
-
-        int n = prices.length;
-
         long NEG = -(long)1e18;
-
-        long[][][] dp =
-            new long[n + 1][k + 1][3];
-
-        // base cases
-        for (int cap = 0; cap <= k; cap++) {
-
-            dp[n][cap][0] = 0;
-
-            dp[n][cap][1] = NEG;
-
-            dp[n][cap][2] = NEG;
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            for (int cap = 1; cap <= k; cap++) {
-
-                // state 0
-                dp[i][cap][0] = Math.max(
-
-                    dp[i + 1][cap][0],
-
-                    Math.max(
-                        -prices[i]
-                        + dp[i + 1][cap][1],
-
-                        prices[i]
-                        + dp[i + 1][cap][2]
-                    )
-                );
-
-                // state 1
-                dp[i][cap][1] = Math.max(
-
-                    dp[i + 1][cap][1],
-
-                    prices[i]
-                    + dp[i + 1][cap - 1][0]
-                );
-
-                // state 2
-                dp[i][cap][2] = Math.max(
-
-                    dp[i + 1][cap][2],
-
-                    -prices[i]
-                    + dp[i + 1][cap - 1][0]
-                );
+        long[][] ahead = new long[k+1][3];
+        long[][] curr = new long[k+1][3];
+        for(int cap = 0; cap<=k;cap++){
+            ahead[cap][0] = 0;
+            ahead[cap][1] = NEG;
+            ahead[cap][2] = NEG;
+        }        
+        for(int i =prices.length - 1;i>=0;i--){
+            for(int cap = 1;cap<=k;cap++){
+                curr[cap][0] = Math.max(ahead[cap][0], Math.max(-prices[i] + ahead[cap][1], prices[i]+ ahead[cap][2]));
+                curr[cap][1] = Math.max(ahead[cap][1], prices[i] + ahead[cap-1][0]);
+                curr[cap][2] = Math.max(ahead[cap][2], -prices[i] + ahead[cap-1][0]);
             }
+            ahead = curr;
+            curr = new long[k+1][3];
         }
-
-        return dp[0][k][0];
+        return ahead[k][0];
     }
 }
