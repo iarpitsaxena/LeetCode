2a97c59f01a31f4dff51c2aaa9f2443271600046
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-06-27 23:07:38 +0530

    Time: 8 ms (95.54%), Space: 48.8 MB (11.1%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,22 +1,13 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        List<int[]> events = new ArrayList<>();
-        for(int[] interval : intervals){
-            events.add(new int[]{interval[0],1});
-            events.add(new int[]{interval[1],-1});
+        if(intervals.length == 0) return 0;
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.offer(intervals[0][1]);
+        for(int i = 1;i < intervals.length;i++){
+            if(intervals[i][0] >= minHeap.peek()) minHeap.poll();
+            minHeap.offer(intervals[i][1]);
         }
-        Collections.sort(events,(a,b)->{
-            if(a[0] == b[0]){
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
-        int rooms = 0;
-        int maxRooms = 0;
-        for(int[] event: events){
-            rooms += event[1];
-            maxRooms = Math.max(rooms,maxRooms);
-        }
-        return maxRooms;
+        return minHeap.size();
     }
 }
