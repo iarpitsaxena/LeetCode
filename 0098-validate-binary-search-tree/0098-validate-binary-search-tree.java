5fa36d85414a5b18b2ab647430f4bac4ad30c1c4
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-07-03 20:35:30 +0530

    Time: 0 ms (100%), Space: 45.3 MB (33.21%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,21 +15,14 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if(root == null) return true;
-        if(root.left!=null && findMax(root.left) >= root.val) return false;
-        if(root.right!= null && findMin(root.right)<= root.val) return false;
-        return isValidBST(root.left) && isValidBST(root.right);
+        return helper(root, null, null);
     }
-    public int findMax(TreeNode node){
-        while(node.right!= null){
-            node = node.right;
-        }
-        return node.val;
-    }
-    public int findMin(TreeNode node){
-        while(node.left!= null){
-            node = node.left;
-        }
-        return node.val;
+    public boolean helper(TreeNode node, Integer low, Integer high){
+        if(node == null) return true;
+        if(low != null && node.val <= low) return false;
+        if(high != null && node.val >= high) return false;
+        boolean leftTree = helper(node.left,low,node.val);
+        boolean rightTree = helper(node.right,node.val, high);
+        return leftTree && rightTree;
     }
 }
