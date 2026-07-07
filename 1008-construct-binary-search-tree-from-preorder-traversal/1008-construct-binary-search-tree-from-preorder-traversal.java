43f0bb41bca31b8757648b9d4b108378cd53167c
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-07-07 14:10:57 +0530

    Time: 1 ms (14.55%), Space: 43.2 MB (94.89%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -1,27 +1,22 @@
-class Solution {
-  public TreeNode bstFromPreorder(int[] preorder) {
-    int n = preorder.length;
-    if (n == 0) return null;
+class Solution{
+    public TreeNode bstFromPreorder(int[] preorder){
+        int n = preorder.length;
+        if(n == 0) return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
+        deque.push(root);
 
-    TreeNode root = new TreeNode(preorder[0]);
-    Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
-    deque.push(root);
+        for(int i = 1;i < n;i++){
+            TreeNode node = deque.peek();
+            TreeNode child = new TreeNode(preorder[i]);
+            while(!deque.isEmpty() && deque.peek().val < child.val){
+                node = deque.pop();
+            }
+            if(node.val < child.val) node.right = child;
+            else node.left = child;
 
-    for (int i = 1; i < n; i++) {
-      //Take the last element of the deque as a parent
-      // and create a child from the next preorder element
-      TreeNode node = deque.peek();
-      TreeNode child = new TreeNode(preorder[i]);
-      //Adjust the parent 
-      while (!deque.isEmpty() && deque.peek().val < child.val)
-        node = deque.pop();
-
-      // follow BST logic to create a parent-child link
-      if (node.val < child.val) node.right = child;
-      else node.left = child;
-      // add the child into deque
-      deque.push(child);
+            deque.push(child);
+        }
+        return root;
     }
-    return root;
-  }
 }
