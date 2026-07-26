4e99ea5a964235266df12f871629fd680ce8f815
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-07-27 00:50:09 +0530

    Time: 0 ms (100%), Space: 42.9 MB (37.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-
-       return solve(nums, 0,dp);
-    }
-    private int solve(int[] nums, int i, int[] dp){
-        if(i >= nums.length){
-            return 0;
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i = 2;i < n;i++){
+            dp[i] = Math.max(dp[i-1],nums[i] + dp[i-2]);
         }
-        if(dp[i]!= -1) return dp[i];
-        int skip = solve(nums,i+1,dp);
-        int rob = solve(nums,i+2,dp) + nums[i];
-        dp[i] =  Math.max(skip,rob);
-        return dp[i];
+        return dp[n-1];
     }
 }
