281694505351352f752e3d1363175ed549eeb8a0
Arpit Saxena <115593255+iarpitsaxena@users.noreply.github.com>
2026-07-26 23:58:08 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,40 +1,18 @@
-// class Solution {
-//     int[] t = new int[101];
-//     int solve(int[] nums, int i, int n){
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
 
-//         if(i >=n) return 0;
-//         if(t[i] != -1) return t[i];
-
-
-//         int steal = nums[i] + solve(nums,i+2,n);
-//         int skip = solve(nums,i+1,n);
-//         return t[i] = Math.max(steal,skip);
-//     }
-//     public int rob(int[] nums) {
-//         int n = nums.length;
-//         Arrays.fill(t,-1);
-//         return solve(nums,0,n);
-//     }
-// }
-
-// // this is done using memoization + recursion(top-down approach).
-
-
-
-class Solution{
-    public int rob(int[] nums){
-        int n = nums.length;
-        if(n == 1) return nums[0];
-        int[] t = new int[n+1];
-        //t[i] = max profit till i house
-        t[0] = 0;
-        t[1] = nums[0];
-        for(int i = 2;i <=n;i++){
-            int steal = nums[i-1] + t[i-2];
-            int skip = t[i-1];
-            t[i] = Math.max(steal,skip);
+       return solve(nums, 0,dp);
+    }
+    private int solve(int[] nums, int i, int[] dp){
+        if(i >= nums.length){
+            return 0;
         }
-        return t[n];
-
+        if(dp[i]!= -1) return dp[i];
+        int skip = solve(nums,i+1,dp);
+        int rob = solve(nums,i+2,dp) + nums[i];
+        dp[i] =  Math.max(skip,rob);
+        return dp[i];
     }
 }
